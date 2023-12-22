9e9f0a5ae82b3c278a3c0fc87bd306a6addfa3b3
Mathias Private Laptop <mathiasberggren1@gmail.com>
2023-12-22 17:22:08 +0100

    Add retries in macos install script

--- macOS/install_script.sh
+++ macOS/install_script.sh
@@ -10,12 +10,21 @@ if ! command -v brew &> /dev/null; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Read the list of formulae from a file
-while read packages; do
-   brew install "$packages"
+# Read the list of packages from a file and install each one individually
+while IFS= read -r package; do
+    for attempt in {1..3}; do
+        if brew install "$package"; then
+            break
+        elif [[ $attempt -eq 3 ]]; then
+            echo "Failed to install $package after $attempt attempts"
+            exit 1
+        fi
+        sleep 5  # wait for 5 seconds before retrying
+    done
 done < brew_packages.txt
 
-# Read the list of casks from a file
-while read cask; do
-   brew install --cask "$cask"
+# Read the list of casks from a file and install each one individually
+while IFS= read -r cask; do
+    brew install --cask "$cask"
 done < brew_casks.txt
+
